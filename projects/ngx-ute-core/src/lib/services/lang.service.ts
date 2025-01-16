08fbe6b6dc8c0b420c31d7cf097a681cc29d5be5
graf-Gopher <sikora_kv@ukr.net>
2025-01-16 18:20:34 +0200

    dev_update_0.5.3-dev.10

--- projects/ngx-ute-core/src/lib/services/lang.service.ts
+++ projects/ngx-ute-core/src/lib/services/lang.service.ts
@@ -102,10 +102,17 @@ export class LangService {
                 }
 
                 // Load library
+                // if (this.environment.ssr) {
                 this.environment.localeList.map(async (x: string) => {
-                    let locale = await import(`../../@angular/common/locales/${this.localeToTag(x)}.mjs`);
+                    let locale = await import(`/node_modules/@angular/common/locales/${this.localeToTag(x)}.mjs`);
                     registerLocaleData(locale.default);
                 });
+                // } else {
+                //     this.environment.localeList.map(async (x: string) => {
+                //         let locale = await import(`../../@angular/common/locales/${this.localeToTag(x)}.mjs`);
+                //         registerLocaleData(locale.default);
+                //     });
+                // }
 
                 await this.loadLocale();
 
